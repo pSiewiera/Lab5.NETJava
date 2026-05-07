cecb80a226f5aefe19438ea13c60b79c6f75527e
pSiewiera <pawel.k.siewiera@gmail.com>
2026-05-07 17:37:58 +0200

    na laby ziutki

---
+++ utils.py
@@ -0,0 +1,19 @@
+"""Moduł z funkcjami kalkulatora."""
+
+def add(a: int, b: int) -> int:
+        """Dodaje dwie liczby."""
+        return a + b
+
+def subtract(a: int, b: int) -> int:
+        """Odejmuje dwie liczby."""
+        return a - b
+
+def multiply(a: int, b: int) -> int:
+        """Mnoży dwie liczby."""
+        return a * b
+
+def divide(a: int, b: int) -> float:
+        """Dzieli dwie liczby."""
+        if b == 0:
+            raise ValueError("Nie dziel przez zero")
+        return a / b
